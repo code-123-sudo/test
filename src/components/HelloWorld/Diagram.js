13431fc11ffc47aa458252b72258ee557a9ec2a3
code-123-sudo <shishirjain899@gmail.com>
2022-12-17 13:53:46 +0530

    fixes

--- src/components/HelloWorld/Diagram.js
+++ src/components/HelloWorld/Diagram.js
@@ -3,10 +3,12 @@ import ReactFlow, {
   Controls,
   Background
 } from "react-flow-renderer";
+import { useZoomPanHelper } from "react-flow-renderer"
 import defaultElements from "./elements";
+//trying to fix useZoomPanhelper import error
 // useZoompanhelper removed , was not able to fix error in import 
 const Diagram = ({ width, height }) => {
-  //const { fitView } = useZoomPanHelper();
+  const { fitView } = useZoomPanHelper();
 
   const [elements, setElements] = React.useState(defaultElements);
 
@@ -18,11 +20,11 @@ const Diagram = ({ width, height }) => {
     <div style={{ height, width }}>
       <ReactFlow
         elements={elements}
-        elementsSelectable={false}
-        nodesConnectable={false}
-        nodesDraggable={false}
-        panOnScroll={false}
-        paneMoveable={false}
+        elementsSelectable={true}
+        nodesConnectable={true}
+        nodesDraggable={true}
+        panOnScroll={true}
+        paneMoveable={true}
       >
         <Background variant="dots" gap={16} size={1} color="#dddddd" />
         <Controls className="react-flow_controls" showInteractive={false} />
--- src/components/HelloWorld/DiagramContainer.js
+++ src/components/HelloWorld/DiagramContainer.js
@@ -5,17 +5,17 @@ import Diagram from "./Diagram";
 
 const DiagramContainer = () => {
   const { innerWidth, innerHeight } = window;
-  const [diagramWidth, setDiagramWidth] = React.useState(300);
-  const [diagramHeight, setDiagramHeight] = React.useState(200);
+  const [diagramWidth, setDiagramWidth] = React.useState(600);
+  const [diagramHeight, setDiagramHeight] = React.useState(600);
 
   return (
     <Rnd
       className="diagram-container"
-      minWidth={300}
-      minHeight={200}
+      minWidth={600}
+      minHeight={600}
       maxWidth={innerWidth}
       maxHeight={innerHeight}
-      default={{ x: 50, y: 50, width: 300, height: 200 }}
+      default={{ x: 50, y: 50, width: 600, height: 600 }}
       disableDragging={true}
       onResizeStop={(e, direction, ref, delta, position) => {
         setDiagramWidth(ref.style.width);
